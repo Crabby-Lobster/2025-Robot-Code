73db2db79a354b2d3c61499b90a2ff66f06bc366
Aiden <133554498+Crabby-Lobster@users.noreply.github.com>
2025-01-16 02:05:08 -0500

    added example trajectory

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -8,8 +8,15 @@ import frc.robot.subsystems.ExampleSubsystem;
 
 import static frc.robot.Constants.OperatorConstants.DrivetrainConstants.*;
 
+import java.util.List;
+
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -42,6 +49,20 @@ public final class Autos {
       .setKinematics(kDriveKinematics)
       // apply voltage constraint
       .addConstraint(autoVoltageConstraint);
+    
+    // test trajectory to follow
+    Trajectory exampleTrajectory = 
+      TrajectoryGenerator.generateTrajectory(
+        //start point
+        new Pose2d(0,0, new Rotation2d(0)),
+        
+        // interior points making S-curve
+        List.of(new Translation2d(0.5,0.5), new Translation2d(1.5,-0.5)),
+
+        // end point 2 meters infront of start point
+        new Pose2d(2,0, new Rotation2d(0)),
+        config
+      );
 
   }
 
