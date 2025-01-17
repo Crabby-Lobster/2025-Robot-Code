345eeaf25c5ef7efa68d6e1094b6f507bc537373
Aiden <133554498+Crabby-Lobster@users.noreply.github.com>
2025-01-16 22:17:26 -0500

    Implemented Sadness mechanic

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -32,6 +32,10 @@ public final class Autos {
   }
 
   public static Command testTrajectory(DriveTrain drivetrain) {
+
+    /** deeer programmer:
+    when i wrote this code, only god and I knew how it worked.
+    Now, only god knows. */
     
     // voltage constraint for ramsete controller
     var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
