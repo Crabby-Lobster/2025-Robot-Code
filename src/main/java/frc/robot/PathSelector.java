9f7a92c35d711f8f87f4a60f8ed0e2990d2ebf57
Consistent-Orange <jcreutter2@gmail.com>
2025-01-20 12:29:11 -0500

    Added Terminal Output

--- src/main/java/frc/robot/PathSelector.java
+++ src/main/java/frc/robot/PathSelector.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 
 /** Add your docs here. */
@@ -33,58 +34,82 @@ public class PathSelector {
                 //to the coral
                 if (rDirect == 90){
                     //right side
+                    SmartDashboard.putString("Pathselector", "Right Coral");
                 }
                 else if(rDirect==270){
                     //left side
+                    SmartDashboard.putString("Pathselector", "Left Coral");
                 }
             }else if (lDirect == 90){
                 //to the right pole
                 //note:right trigger makes it go to the bottom of l/r sides
-                
+
                 if (rDirect == 0){
                     //1st side aka top
+                    SmartDashboard.putString("Pathselector", "Top Reef Right Pole");
                 }
                 else if (rDirect == 90){
                     //right side
                     if (rJoystick.getRawButtonPressed(0)){
                         //3th side aka bottom right
+                        SmartDashboard.putString("Pathselector", "Bottom Right Reef Right Pole");
                     }else{
                         //2nd side aka top right
+                        SmartDashboard.putString("Pathselector", "Top Right Reef Right Pole");
+
                     }
                 }
                 else if (rDirect == 180){
                     //4th side aka bottom
+                    SmartDashboard.putString("Pathselector", "Bottom Reef Right Pole");
+
                 }
                 else if(rDirect==270){
                     //left side
                     if (rJoystick.getRawButtonPressed(0)){
                         //5th side aka bottom left
+                        SmartDashboard.putString("Pathselector", "Bottom Left Reef Right Pole");
+
                     }else{
                         //6th side aka top left
+                        SmartDashboard.putString("Pathselector", "Top Left Reef Right Pole");
+
                     }
                 }
             }else if (lDirect == 270){
                 //to the left pole
                 if (rDirect == 0){
                     //1st side aka top
+                    SmartDashboard.putString("Pathselector", "Top Reef Left Pole");
+
                 }
                 else if (rDirect == 90){
                     //right side
                     if (rJoystick.getRawButtonPressed(0)){
                         //3th side aka bottom right
+                        SmartDashboard.putString("Pathselector", "Bottom Right Reef Left Pole");
+
                     }else{
                         //2nd side aka top right
+                        SmartDashboard.putString("Pathselector", "Top Right Reef Left Pole");
+
                     }
                 }
                 else if (rDirect == 180){
                     //4th side aka bottom
+                    SmartDashboard.putString("Pathselector", "Bottom Reef Left Pole");
+
                 }
                 else if(rDirect==270){
                     //left side
                     if (rJoystick.getRawButtonPressed(0)){
                         //5th side aka bottom left
+                        SmartDashboard.putString("Pathselector", "Bottom Left Reef Left Pole");
+
                     }else{
                         //6th side aka top left
+                        SmartDashboard.putString("Pathselector", "Top Left Reef Left Pole");
+
                     }
                 }
             }
