06a012fe1f4aac74bea58cdfc080f94383ecc50f
Aiden <133554498+Crabby-Lobster@users.noreply.github.com>
2025-01-22 17:17:55 -0500

    added default elevator

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,10 @@ package frc.robot;
 import frc.robot.Constants.ControllerConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.DefaultDrive;
+import frc.robot.commands.DefaultElevator;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.ExampleSubsystem;
 import choreo.auto.AutoFactory;
 import edu.wpi.first.wpilibj.Joystick;
@@ -34,8 +36,10 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final DriveTrain m_driveTrain = new DriveTrain();
+  private  final Elevator m_elevator = new Elevator();
 
   private final DefaultDrive m_DefaultDrive = new DefaultDrive(leftStick, rightStick, m_driveTrain, controller);
+  private final DefaultElevator m_DefaultElevator = new DefaultElevator(m_elevator, leftStick, rightStick, controller);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -55,6 +59,7 @@ public class RobotContainer {
     configureBindings();
 
     m_driveTrain.setDefaultCommand(m_DefaultDrive);
+    m_elevator.setDefaultCommand(m_DefaultElevator);
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/DefaultElevator.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class DefaultElevator extends Command {
+  Joystick leftJoystick;
+  Joystick rightJoystick;
+  XboxController controller;
+
+  Elevator elevator;
+  /** Creates a new DefaultElevator. */
+  public DefaultElevator(Elevator elevator, Joystick leftstick, Joystick rightstick, XboxController controller) {
+    this.elevator = elevator;
+
+    this.leftJoystick = leftstick;
+    this.rightJoystick = rightstick;
+    this.controller = controller;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(elevator);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    elevator.setSpeed(0.25);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
