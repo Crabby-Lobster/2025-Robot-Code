c006595ce2d5e82378dd1609c5a0e24dd9c6c9bc
Aiden <133554498+Crabby-Lobster@users.noreply.github.com>
2025-03-10 13:18:17 -0400

    remnoved unused import

--- src/main/java/frc/robot/commands/DefaultScoreSystem.java
+++ src/main/java/frc/robot/commands/DefaultScoreSystem.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.PositionContainer;
-import frc.robot.ScoreSystemState;
 import frc.robot.subsystems.ScoreSystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
