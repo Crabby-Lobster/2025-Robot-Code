c58a17492aced39b4243be18fe36dfa71aeed816
Aiden <133554498+Crabby-Lobster@users.noreply.github.com>
2025-03-19 18:30:01 -0400

    changed angle

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -106,7 +106,7 @@ public final class Constants {
     public static final double[] dangerHeight = {0,10};
 
 
-    public static final double Barge = 65;
+    public static final double Barge = 70;
     public static final double ReefHigh = 0;
     public static final double ReefLow = -12;
     public static final double STORE = 65;
